7ba149b4e5fc9b93ac6418741016ffb95f08de36
Nadilson <nadilson@protonmail.com>
2022-11-03 09:02:57 -0300

    Finalização do setup do back-end (contagem de bolões e diagrama ERD)

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,13 +1,29 @@
 import Fastify from "fastify";
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient({
+    log: ["query"],
+})
 
 async function bootstrap() {
     const fastify = Fastify({
         logger: true,
     })
 
-    fastify.get("/pools/count", () => {
+    fastify.get("/pools/count", async () => {
+        /* const pools = await prisma.pool.findMany({
+            where: {
+                code: {
+                    startsWith: 'N'
+                }
+            }
+        })
         return {
-            count: 128931283
+            pools
+        } */
+        const count = await prisma.pool.count()
+        return {
+            count
         }
     }) 
 
