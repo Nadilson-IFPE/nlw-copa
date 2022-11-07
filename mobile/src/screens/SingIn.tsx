c6585aa6c11f6147b1b2c05733b0f4d616ef5f3e
Nadilson <nadilson@protonmail.com>
2022-11-07 06:22:09 -0300

    Configuração do mobile + componentes SingIn e Loading

---
+++ mobile/src/screens/SingIn.tsx
@@ -0,0 +1,9 @@
+import { Center, Text } from 'native-base';
+
+export function SingIn() {
+    return (
+        <Center flex={1} bgColor="grey.900">
+            <Text color="white" fontSize={24} fontFamily="heading">SignIn</Text>
+        </Center>
+    );
+}
